5245b961b274976e168b17a81f2ca9612516be04
yamuna898 <yamunar845@gmail.com>
2025-11-24 18:57:42 +0530

    Updated code without;

--- Assignments/noSemicolon.c
+++ Assignments/noSemicolon.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main(){
-    char name='Yamuna';
-    if(name){printf(name);}
+    if(printf("Yamuna\n")) 
     return 0;
 }
+
+/*
+Sir, I first tried the code that you've wrote on board
+but it was not printing without semicolon.
+
+#include <stdio.h>
+int main(){
+    char name = 'Yamuna';   // this wasn't working properly
+    if(name){ printf(name) }  //ERROR here "missing semicoln"
+    return 0;
+}
+
+In lab sir told we can use scanf inside if,
+so I tried same with printf and it worked without a semicoln.
+*/
