9cfa1c98f2fafda4d63adeda754223411397768b
yamuna898 <yamunar845@gmail.com>
2025-12-06 18:53:26 +0530

    Its working thus Complete

--- Assignments/WaysToMultiMatrices.c
+++ Assignments/WaysToMultiMatrices.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int ways(int n) {
+    int C[n];
+    C[0] = 1;
+    C[1] = 1;
+
+    for (int i = 2; i <= n-1; i++) {
+        C[i] = 0;
+        for (int j = 0; j < i; j++) {
+            C[i] += C[j] * C[i - 1 - j];
+        }
+    }
+    return C[n-1];
+}
+
+int main() {
+    int m;
+
+    printf("Enter number of matrices: ");
+    scanf("%d", &m);
+
+    printf("\n===========Order of all matrix should be same==============\n");
+
+    printf("\nNumber of possible ways to multiply %d matrices: %d\n", m,ways(m));
+
+    return 0;
+}
